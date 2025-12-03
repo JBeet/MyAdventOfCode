648d3256beb351529e5a2fa366522ccd9c6eb9f7
JaapBeetstra <jaap@app-it-up.com>
2025-12-03 17:09:46 +0100

    Day 03 visualization - Improved colors

--- src/aoc2025/Day03_visual.kt
+++ src/aoc2025/Day03_visual.kt
@@ -41,7 +41,7 @@ sealed interface PuzzleSolver {
     val outputSize: Int
     val activeBank: Bank?
     val isFinished: Boolean
-    val activeItems: List<SolutionRow>
+    val activeItems: List<WhiteboardRow>
     suspend fun next(): PuzzleSolver
     fun digitColor(idx: Int): Color
     val delay: Int
@@ -50,7 +50,7 @@ sealed interface PuzzleSolver {
         override val activeBank: Bank? = null
         override val isFinished = true
         override val outputSize: Int = 2
-        override val activeItems: List<SolutionRow> = emptyList()
+        override val activeItems: List<WhiteboardRow> = emptyList()
         override suspend fun next(): PuzzleSolver = this
         override fun digitColor(idx: Int): Color = Color.Transparent
         override val delay: Int = 0
@@ -60,14 +60,14 @@ sealed interface PuzzleSolver {
         val parts: List<BankSolver>,
         val active: BankSolver,
         val total: Long,
-        val completed: List<SolutionRow.BankCompleted>
+        val completed: List<WhiteboardRow.BankCompleted>
     ) : PuzzleSolver {
         constructor(parts: List<BankSolver>) : this(parts.drop(1), parts[0], 0, emptyList())
 
         override val activeBank: Bank = active.bank
         override val isFinished: Boolean = false
         override val outputSize: Int = active.outputSize
-        override val activeItems: List<SolutionRow> get() = completed + SolutionRow.RunningTotal(total) + active.items
+        override val activeItems: List<WhiteboardRow> get() = completed + WhiteboardRow.RunningTotal(total) + active.items
         override suspend fun next(): PuzzleSolver {
             if (active !is BankSolver.Completed)
                 return Sum(parts, active.next(), total, completed)
@@ -83,69 +83,66 @@ sealed interface PuzzleSolver {
     }
 
     data class Completed(
-        override val outputSize: Int, val total: Long, val completed: List<SolutionRow.BankCompleted>
+        override val outputSize: Int, val total: Long, val completed: List<WhiteboardRow.BankCompleted>
     ) : PuzzleSolver {
         override val activeBank: Bank? = null
         override val isFinished: Boolean = true
-        override val activeItems: List<SolutionRow> = completed + listOf(SolutionRow.Completed(total))
+        override val activeItems: List<WhiteboardRow> = completed + listOf(WhiteboardRow.Completed(total))
         override suspend fun next(): PuzzleSolver = this
         override fun digitColor(idx: Int): Color = Color.Transparent
         override val delay: Int = 0
     }
 }
 
-sealed interface SolutionRow {
+sealed interface WhiteboardRow {
     val size: Int
     operator fun get(idx: Int): Char
     fun color(idx: Int): Color
-    data class RunningTotal(val total: Long) : SolutionRow {
+    data class RunningTotal(val total: Long) : WhiteboardRow {
         val s = total.toString()
         override val size: Int get() = s.length
         override fun get(idx: Int): Char = s[idx]
         override fun color(idx: Int): Color = Color.Green
     }
 
-    data class BankCompleted(val src: Partial) : SolutionRow by src {
+    data class BankCompleted(val src: Partial) : WhiteboardRow by src {
         val value = src.value
         override fun color(idx: Int): Color = Color.Cyan
     }
 
-    data class Completed(val total: Long) : SolutionRow {
+    data class Completed(val total: Long) : WhiteboardRow {
         val s get() = total.toString()
         override val size: Int get() = s.length
         override fun get(idx: Int): Char = s[idx]
         override fun color(idx: Int): Color = Color.Green
     }
 
-    data class Partial(val bank: Bank, val indices: List<Int>) : SolutionRow {
+    data class Partial(val bank: BankSolver, val indices: List<Int>) : WhiteboardRow {
         override val size: Int get() = indices.size
         val value get() = indices.fold(0L) { acc, idx -> acc * 10 + bank[idx] }
         override fun get(idx: Int): Char = bank[indices[idx]].digitToChar()
-        override fun color(idx: Int): Color {
-            val value = (1f + idx) / indices.size
-            return Color(value, 0f, value, 0.5f)
-        }
+        override fun color(idx: Int): Color = bank.color(indices[idx])
     }
 }
 
 sealed interface BankSolver {
     val bank: Bank
     val outputSize: Int
-    val items: List<SolutionRow>
+    val items: List<WhiteboardRow>
     suspend fun next(): BankSolver
     fun color(idx: Int): Color
     val delay: Int
+    operator fun get(idx: Int): Int = bank[idx]
 
     data class Initial(override val bank: Bank, override val outputSize: Int) : BankSolver {
-        override val items: List<SolutionRow> = emptyList()
+        override val items: List<WhiteboardRow> = emptyList()
         override suspend fun next(): BankSolver = GrowStart(bank, outputSize)
         override fun color(idx: Int): Color = Color.Transparent
         override val delay: Int = 2
     }
 
-    data class GrowStart(override val bank: Bank, override val outputSize: Int) :
-        BankSolver {
-        override val items: List<SolutionRow.Partial> = listOf(SolutionRow.Partial(bank, listOf(0)))
+    data class GrowStart(override val bank: Bank, override val outputSize: Int) : BankSolver {
+        override val items: List<WhiteboardRow.Partial> = listOf(WhiteboardRow.Partial(this, listOf(0)))
         override suspend fun next(): BankSolver = GrowMoreBefore(bank, outputSize, items, 1)
         override fun color(idx: Int): Color = if (idx == 0) Color(1f, 0f, 1f, 0.5f) else Color.Transparent
         override val delay: Int = 1
@@ -154,7 +151,7 @@ sealed interface BankSolver {
     data class GrowMoreBefore(
         override val bank: Bank,
         override val outputSize: Int,
-        override val items: List<SolutionRow.Partial>,
+        override val items: List<WhiteboardRow.Partial>,
         val index: Int
     ) : BankSolver {
         init {
@@ -167,23 +164,23 @@ sealed interface BankSolver {
         override suspend fun next(): BankSolver =
             GrowMoreAfter(bank, outputSize, nextItems(), index)
 
-        private fun nextItems(): List<SolutionRow.Partial> = buildList {
+        private fun nextItems(): List<WhiteboardRow.Partial> = buildList {
             val newDigit = bank[index]
             if (newDigit > items[0].value)
-                add(SolutionRow.Partial(bank, listOf(index)))
+                add(WhiteboardRow.Partial(this@GrowMoreBefore, listOf(index)))
             else
-                add(items[0])
+                add(items[0].copy(bank = this@GrowMoreBefore))
             (2..minOf(index, outputSize)).forEach { size ->
                 val known = items[size - 1]
                 val smaller = items[size - 2]
                 val newValue = smaller.value * 10 + newDigit
                 if (newValue > known.value)
-                    add(SolutionRow.Partial(bank, smaller.indices + listOf(index)))
+                    add(WhiteboardRow.Partial(this@GrowMoreBefore, smaller.indices + listOf(index)))
                 else
-                    add(known)
+                    add(known.copy(bank = this@GrowMoreBefore))
             }
             if (index < outputSize)
-                add(SolutionRow.Partial(bank, items.last().indices + listOf(index)))
+                add(WhiteboardRow.Partial(this@GrowMoreBefore, items.last().indices + listOf(index)))
         }
 
         val usedIndices = items.flatMapTo(mutableSetOf()) { it.indices }.sorted()
@@ -202,7 +199,7 @@ sealed interface BankSolver {
     data class GrowMoreAfter(
         override val bank: Bank,
         override val outputSize: Int,
-        override val items: List<SolutionRow.Partial>,
+        override val items: List<WhiteboardRow.Partial>,
         val index: Int
     ) : BankSolver {
         init {
@@ -216,7 +213,7 @@ sealed interface BankSolver {
             if (index < bank.size - 1)
                 GrowMoreBefore(bank, outputSize, items, index + 1)
             else
-                Completed(bank, outputSize, SolutionRow.BankCompleted(items.last()))
+                Completed(bank, outputSize, WhiteboardRow.BankCompleted(items.last()))
 
         val usedIndices = items.flatMapTo(mutableSetOf()) { it.indices }.sorted()
         override fun color(idx: Int): Color {
@@ -234,11 +231,11 @@ sealed interface BankSolver {
     data class Completed(
         override val bank: Bank,
         override val outputSize: Int,
-        val solution: SolutionRow.BankCompleted
+        val solution: WhiteboardRow.BankCompleted
     ) :
         BankSolver {
         val value: Long get() = solution.value
-        override val items: List<SolutionRow> = listOf(SolutionRow.RunningTotal(value))
+        override val items: List<WhiteboardRow> = listOf(WhiteboardRow.RunningTotal(value))
         override suspend fun next(): BankSolver = this
         override fun color(idx: Int): Color = Color.Transparent
         override val delay: Int = 5
